2b52f14aad92c4a2036ec392a4d5e0ebd8903644
nidhibali23 <balinidhu2000@gmail.com>
2025-05-06 09:48:21 +0530

    latest task added to create task

--- src/auth/ProtectedRoute.jsx
+++ src/auth/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import React, { children, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
-const ProtectedRoute = () => {
+const ProtectedRoute = ({ children }) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const navigate = useNavigate();
 
--- src/pages/CreateTask.jsx
+++ src/pages/CreateTask.jsx
@@ -2,7 +2,40 @@ import React from 'react'
 
 const CreateTask = () => {
     return (
-        <div>CreateTask</div>
+        <div className='container-fluid h-100'>
+            <div className='row h-100'>
+                <div className='col-lg-6 d-flex align-items-center 
+                       justify-content-center h-100 flex-column bg-primary text-white'>
+                    <h1 className='text-center mb-3 text-uppercase display-6'>
+                        An App to<br />
+                        make your life<br />
+                        <span className='display-2'>Organized</span>
+                    </h1>
+
+                    <img src={illustration} />
+
+                </div>
+
+                <div className='col-lg-6 d-flex align-items-center justify-content-center h-g-100 flex-column'>
+
+                    <div className='card rounded-0 w-50'>
+                        <div className='card-header d-flex align-items-center p-0 text-center'>
+                            <NavLink to="/login" className={`w-50 p-2 align-items-center bg-primary text-white`}>Login</NavLink>
+                            <NavLink to="/register" className={`w-50 p-2 align-items-center bg-primary text-white`}>Register</NavLink>
+
+                        </div>
+                        <div className='card-body'>
+                            <Outlet />
+                        </div>
+
+                    </div>
+
+
+                </div>
+            </div>
+
+
+        </div>
     )
 }
 
